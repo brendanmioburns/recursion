5e6d72f411370306ed84b7cc756c55e64337d54a
Brendan Burns <brendanmioburns@gmail.com>
2017-05-07 12:59:11 -0700

    finished and added concise explanations in comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+
+  var result = [];
+  var selectedNode = node || document.body;
+  var selectedNodeChildren = selectedNode.childNodes;
+
+//if the node or document body has class names listed, and the className parameter is found within them, push the element matching the className to result
+  if (selectedNode.classList && selectedNode.classList.contains(className)) {
+    result.push(selectedNode);
+  }
+//if the element matching the className has child nodes, execute getElementsByClassName on all of them until it runs out of child nodes
+  if (selectedNodeChildren) {
+    selectedNodeChildren.forEach(function(selectedNodeChild) {
+      result = result.concat(getElementsByClassName(className, selectedNodeChild));
+    })
+  }
+  return result;
 };
